10451c71b66ff1d8ac5be5a3c13623a262ed42ab
Pedro Vinícius de Melo Silva <65138009+PedroViniciusMelo@users.noreply.github.com>
2021-04-15 09:32:03 -0300

    Added encryption fuction to user update method

--- src/domain/usercase/user/updateUser.ts
+++ src/domain/usercase/user/updateUser.ts
@@ -1,6 +1,7 @@
 import { User } from "@domain/entity";
 import { IUserGateway } from "@src/data/gateway";
 import { IValidation } from "@src/helper/validations"
+import * as bcrypt from "bcrypt";
 
 
 export class UpdateUser {
@@ -12,6 +13,8 @@ export class UpdateUser {
     }
 
     async update(id: number, user: User): Promise<User> {
+        let hashedPassword = await bcrypt.hash(user.password, 10);
+        user.password = hashedPassword;
         return this.userGateway.update(id, user);
     }
 }
